7e2bf4bd9d27aa04bae3822eeb30700b5aba4521
TatianaYakushonok <yakush2@yandex.ru>
2023-12-14 13:54:01 +0700

    feat: add task (Thu, 14 Dec 2023 13:54:00 +0700)

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 'use strict';
 
-const ads = document.querySelector('.ads');
-const items = document.querySelector('.items');
-const item = document.querySelectorAll('.item');
-const content = document.querySelectorAll('.content');
-const propsList = document.querySelectorAll('.props__list');
-const propsItem = document.querySelectorAll('.props__item');
-const propsItemTwo = document.querySelectorAll('.props__item_two');
-const itemTitle = document.querySelectorAll('.item__title');
+const ul = document.createElement('ul');
+document.querySelector('.container').append(ul);
 
-ads.remove();
-items.append(item[1], item[2], item[3], item[0], item[4], item[5]);
-content[4].append(itemTitle[4], propsList[3]);
-propsItem[2].after(propsItem[14]);
-propsList[2].before(itemTitle[3]);
-propsList[5].before(itemTitle[1]);
-itemTitle[2].textContent = 'This и прототипы объектов';
-itemTitle[2].after(propsList[4]);
-Array.from(propsItemTwo).map((item, i) => {
-  if (i === 8 || i === 9) propsList[2].append(item);
-});
+const checkingString = (el, str) => {
+  if (str === 'del') el.lastChild.remove();
+  if (str === 'clear') el.replaceChildren();
+  if (str === null || str === 'exit') return null;
+
+  if (!isNaN(str) || str === 'del' || str === 'clear') {
+    return false;
+  }
+
+  return true;
+};
+
+const addStr = () => {
+  const str = prompt('Введите текст для пункта списка');
+  return str;
+};
+
+const createElement = () => {
+  const str = addStr();
+  const checkStr = checkingString(ul, str);
+
+  if (checkStr) {
+    ul.innerHTML += `<li>${str}</li>`;
+  }
+  if (checkStr === null) {
+    return;
+  }
+
+  return createElement();
+};
+
+createElement();
